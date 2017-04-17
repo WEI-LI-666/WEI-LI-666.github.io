870f248a219cc75a1178099eddc98c08f07e0e8e
WEI-LI-666 <zxc851023@gmail.com>
2017-04-17 22:28:03 +0800

    my site

---
+++ php/submit.php
@@ -0,0 +1,15 @@
+
+<!-- saved from url=(0065)http://demos.webicode.com/html/zap/one-pages-demos/php/submit.php -->
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<script language="javascript" type="text/javascript">
+		window.alert("Sent Successfuly.");
+	</script>
+</head>
+<body>
+	<div id="window-resizer-tooltip"><a href="http://demos.webicode.com/html/zap/one-pages-demos/php/submit.php#" title="Edit settings"></a>
+	<span class="tooltipTitle">Window size: </span><span class="tooltipWidth" id="winWidth"></span> x <span class="tooltipHeight" id="winHeight"></span><br><span class="tooltipTitle">Viewport size: </span><span class="tooltipWidth" id="vpWidth"></span> x <span class="tooltipHeight" id="vpHeight"></span>
+	</div
+</body>
+</html>
